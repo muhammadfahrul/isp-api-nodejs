1c64e74431de12a059812d89a8213e53fdc46a37
muhammadfahrul <muhammadfahrul481@gmail.com>
2019-10-15 09:44:59 +0700

    Add Exception Handler

--- app/Controllers/Http/SaranaController.js
+++ app/Controllers/Http/SaranaController.js
@@ -1,24 +1,80 @@
 'use strict'
 
+const Sarana = use('App/Models/Sarana')
+const {validate} = use('Validator')
+
 class SaranaController {
     async showSarana({request, response}) {
-
+        const sarana = await Sarana.all()
+        
+        return response.json({
+            status: true,
+            data: sarana
+        })
     }
 
     async saranaId({request, response}) {
-        
+        const saranaId = await Sarana.find(request.params.id)
+
+        return response.json({
+            status: true,
+            data: saranaId
+        })
     }
 
     async addSarana({request, response}) {
-        
+        const rules = {
+            nama: 'required|unique',
+            is_active : 'required'
+        }
+
+        const validation = await validate(request.all(), rules)
+
+        if(validation.fails()) {
+            return response.json({
+                status: false,
+                message: validation.messages()
+            })
+        }
+
+        const addSarana = new Sarana()
+        addSarana.nama = request.body.nama
+        addSarana.is_active = request.body.is_active
+        await addSarana.save()
+
+        return response.json({
+            status: true,
+            message: 'Created Sarana Successfully',
+            data: addSarana
+        })
     }
 
     async editSarana({request, response}) {
-        
+        // let saranaId = request.params.id
+
+        // if(!saranaId == request.all(id)) {
+
+        // }
+
+        const editSarana = await Sarana.find(request.params.id)
+        editSarana.nama = request.body.nama
+        editSarana.is_active = request.body.is_active
+        await editSarana.save()
+
+        return response.json({
+            status: true,
+            message: 'Updated Sarana Successfully'
+        })
     }
 
     async deleteSarana({request, response}) {
-        
+        const deleteSarana = await Sarana.find(request.params.id)
+        await deleteSarana.delete()
+
+        return response.json({
+            status: true,
+            message: 'Deleted Sarana Successfully'
+        })
     }
 }
 
