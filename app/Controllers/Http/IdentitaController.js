5ea9a7a370b2959f86b50bf4afdab3071ceabf89
Ayu Intan <ayuintanpm@gmail.com>
2019-10-22 14:38:08 +0700

    Crud Identitas

--- app/Controllers/Http/IdentitaController.js
+++ app/Controllers/Http/IdentitaController.js
@@ -54,7 +54,7 @@ class IdentitaController {
         })
 
         await myPicture.move(Helpers.publicPath('uploads/identitas'))
-        identitas.gambar = new Date()+getTime()+'.'+myPicture.subtype
+        identitas.gambar = new Date().getTime()+'.'+myPicture.subtype
 
         identitas.deskripsi = request.body.deskripsi        
 
@@ -68,20 +68,45 @@ class IdentitaController {
     }
 
     async editIdentitas({request, response, params}) {
-        const identitas_req = request.only(['klasifikasi_id', 'tahun', 'nomor_urut'])
-        const identitas = await Identitas.find(params.id)
-        if (!identitas) {
-            return response.status(404).json({data: 'Role not found'})
+        const rules = {
+            user_id: 'required|exists:users,id',
+            klasifikasi_id : 'required|exists:klasifikasi,id',
+            tahun : 'required',
+            nomor_urut : 'required',
+            deskripsi: 'required'
         }
-        identitas.klasifikasi_id = identitas_req.klasifikasi_id
-        identitas.tahun = identitas_req.tahun
-        identitas.nomor_urut = identitas_req.nomor_urut
-        await identitas.save()
+
+        // const identitas_req = request.only(['klasifikasi_id', 'tahun', 'nomor_urut'])
+        const validation = await validate(request.all(), rules)
+
+        if (validation.fails()) {
+            return response.json({
+                message : validation.messages()
+        })
+        }
+
+        const identitas = await Identitas.find(params.id)
+        identitas.user_id = request.body.user_id
+        identitas.klasifikasi_id = request.body.klasifikasi_id
+        identitas.tahun = request.body.tahun
+        identitas.nomor_urut = request.body.nomor_urut
+
+        const myPicture = request.file('gambar', {
+            types: ['image'],
+            size: '2mb'
+        })
+
+        await myPicture.move(Helpers.publicPath('uploads/identitas'))
+        identitas.gambar = new Date().getTime()+'.'+myPicture.subtype
+
+        identitas.deskripsi = request.body.deskripsi        
+
+        await identitas.save();
 
         return response.json({
             status: 400,
-            message: 'Updated Identitas Successfully',
-            data: identitas 
+            message: 'Created Identitas Successfully',
+            data: identitas  
         })
     }
 
